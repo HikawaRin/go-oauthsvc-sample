46a74ee6b0cd50d7f09fc17248461176ba7013b1
Hikawa Rin <releaseavalon@gmail.com>
2022-10-11 08:57:02 +0000

    Save token uuid to Redis

--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v7"
+	"github.com/twinj/uuid"
 )
 
 var (
@@ -18,9 +21,12 @@ var (
 		Username: "username",
 		Password: "passwd",
 	}
+
+	client *redis.Client
 )
 
 func main() {
+	init_redis()
 	router.POST("/login", Login)
 	log.Fatal(router.Run(":8080"))
 }
@@ -41,13 +47,22 @@ func Login(c *gin.Context) {
 		c.JSON(http.StatusUnauthorized, "Unkown User")
 		return
 	}
-	token, err := CreateToken(u.ID)
+	// token, err := CreateToken(u.ID)
+	ts, err := CreateTokenV2(u.ID)
 	if err != nil {
 		c.JSON(http.StatusUnprocessableEntity, err.Error())
 		return
 	}
+
+	saveErr := CreateAuth(u.ID, ts)
+	if saveErr != nil {
+		c.JSON(http.StatusUnprocessableEntity, saveErr.Error())
+	}
+	token := map[string]string{
+		"access_token":  ts.AccessToken,
+		"refresh_token": ts.RefreshToken,
+	}
 	c.JSON(http.StatusOK, token)
-	log.Print("Leave Func Login")
 }
 
 func isUser(u *User) bool {
@@ -60,7 +75,7 @@ func isUser(u *User) bool {
 func CreateToken(uid uint64) (string, error) {
 	var err error
 
-	os.Setenv("ACCESS_SECRET", "1145141919810")
+	os.Setenv("ACCESS_SECRET", "114514")
 	atClaims := jwt.MapClaims{}
 	atClaims["authorized"] = true
 	atClaims["user_id"] = uid
@@ -68,4 +83,71 @@ func CreateToken(uid uint64) (string, error) {
 	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
 	token, err := at.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
 	return token, err
+}
+
+func init_redis() {
+	dsn := os.Getenv("REDIS_DSN")
+	if len(dsn) == 0 {
+		dsn = "localhost:6379"
+	}
+	client = redis.NewClient(&redis.Options{
+		Addr: dsn,
+	})
+	_, err := client.Ping().Result()
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+type TokenDetail struct {
+	AccessToken  string
+	RefreshToken string
+	AccessUuid   string
+	RefreshUuid  string
+	AtExpires    int64
+	RtExpires    int64
+}
+
+func CreateTokenV2(uid uint64) (*TokenDetail, error) {
+	var err error
+
+	td := &TokenDetail{}
+	td.AtExpires = time.Now().Add(time.Minute * 15).Unix()
+	td.AccessUuid = uuid.NewV4().String()
+	td.AccessToken, err = CreateToken(uid)
+	if err != nil {
+		return nil, err
+	}
+
+	td.RtExpires = time.Now().Add(time.Hour).Unix()
+	td.RefreshUuid = uuid.NewV4().String()
+
+	os.Setenv("REFRESH_SECRET", "1919810")
+	rtClaims := jwt.MapClaims{}
+	rtClaims["refresh_uuid"] = td.RefreshUuid
+	rtClaims["user_id"] = uid
+	rtClaims["exp"] = td.RtExpires
+	rt := jwt.NewWithClaims(jwt.SigningMethodHS256, rtClaims)
+	td.RefreshToken, err = rt.SignedString([]byte(os.Getenv("REFRESH_SECRET")))
+	if err != nil {
+		return nil, err
+	}
+	return td, nil
+}
+
+func CreateAuth(uid uint64, td *TokenDetail) error {
+	at := time.Unix(td.AtExpires, 0)
+	rt := time.Unix(td.RtExpires, 0)
+	now := time.Now()
+
+	errAccess := client.Set(td.AccessUuid, strconv.Itoa(int(uid)), at.Sub((now))).Err()
+	if errAccess != nil {
+		return errAccess
+	}
+
+	errRefresh := client.Set(td.RefreshUuid, strconv.Itoa(int(uid)), rt.Sub((now))).Err()
+	if errRefresh != nil {
+		return errRefresh
+	}
+	return nil
 }
